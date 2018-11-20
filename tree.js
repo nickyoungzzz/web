b0ee0614f8bcce188d65d64de8d7dc5c567ed4c0
nickyoungzzz <1085823261@qq.com>
2018-11-20 15:04:28 +0800

    日常修改

--- tree.js
+++ tree.js
@@ -26,8 +26,8 @@ class Node {
                 node.qx = node.x + 14;
                 node.qy = node.y - 14;
                 node.pv = this.value;
-                this.left = node;
                 node.parent = this;
+                this.left = node;
             }
         }
         else if (node.value > this.value) {
@@ -48,84 +48,34 @@ class Node {
         }
     }
 
-    delete(node)
-    {
-        if (node.value < this.value)
-        {
-            if (this.left != null)
-            {
+    delete(node) {
+        if (node.value < this.value) {
+            if (this.left != null) {
                 this.left.delete(node);
             }
-            else
-            {
+            else {
                 throw new Error('无此节点');
             }
         }
-        else if (node.value > this.value)
-        {
-            if (this.right != null)
-            {
+        else if (node.value > this.value) {
+            if (this.right != null) {
                 this.right.delete(node);
             }
-            else
-            {
+            else {
                 throw new Error('无此节点');
             }
         }
-        else
-        {
-            if (this.parent.left != null && this.parent.left.value == this.value)
-            {
+        else {
+            if (this.parent.left != null && this.parent.left.value == this.value) {
                 this.parent.left = null;
             }
-            if (this.parent.right != null && this.parent.right.value == this.value)
-            {
+            if (this.parent.right != null && this.parent.right.value == this.value) {
                 this.parent.right = null;
             }
             this.parent = null;
             return;
         }
     }
-
-    push(node, callback) {
-
-        if (node.value < this.value) {
-            if (this.left != null) {
-                this.left.add(node);
-            }
-            else {
-                node.x = this.x - 100;
-                node.y = this.y + 100;
-                node.px = this.x - 14;
-                node.py = this.y + 14;
-                node.qx = node.x + 14;
-                node.qy = node.y - 14;
-                node.pv = this.value;
-                this.left = node;
-                node.parent = this;
-                callback(node);
-            }
-        }
-        else if (node.value > this.value) {
-            if (this.right != null) {
-                this.right.add(node);
-            }
-            else {
-                node.x = this.x + 100;
-                node.y = this.y + 100;
-                node.px = this.x + 14;
-                node.py = this.y + 14;
-                node.qx = node.x - 14;
-                node.qy = node.y - 14;
-                node.pv = this.value;
-                node.parent = this;
-                this.right = node;
-                callback(node);
-            }
-        }
-
-    }
-
 }
 
 class Tree {
@@ -174,40 +124,23 @@ class Tree {
 
     draw1(callback) {
 
-        if (this.head != null)
-        {
+        if (this.head != null) {
             this.draw(this.head, callback);
         }
     }
 
-    push(n, callback) {
+    delete(n) {
         let node = new Node(n);
         if (this.head == null) {
-            this.head = node;
-            callback(node);
-        }
-        else {
-            this.head.push(node, callback);
-        }
-    }
-
-    delete(n)
-    {
-        let node = new Node(n);
-        if (this.head == null)
-        {
             throw new Error('无根节点');
         }
-        else
-        {
-            if (this.head.value == n)
-            {
-                this.head = null;
+        else {
+            if (this.head.value == n) {
                 this.head.left = null;
                 this.head.right = null;
+                this.head = null;
             }
-            else
-            {
+            else {
                 this.head.delete(node);
             }
         }
@@ -215,20 +148,4 @@ class Tree {
 
 }
 
-function run(array) {
-    let tree = new Tree();
-    for (let index = 0; index < array.length; index++) {
-        const element = array[index];
-        tree.add(element);
-    }
-    return tree;
-}
 
-function readOnly(obj) {
-    return function (e) {
-        return obj[`${e}`];
-    }
-}
-const read = readOnly({ color: 'red', size: 100 });
-console.log(read('color'));
-console.log(read('size'));
